35bd5aeb9d61443f5b2ec8412e8e44f8dbc7bf32
Kelvin Montini <kelvin.montini@outlook.com>
2020-01-03 16:58:56 -0300

    Configurações do DB e migration do usuário

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'rajje.db.elephantsql.com',
+  username: 'qjejkrmy',
+  password: '3VQVJCN8oUlC9KwyQNYElfFaHV9G3O-5',
+  database: 'qjejkrmy',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
