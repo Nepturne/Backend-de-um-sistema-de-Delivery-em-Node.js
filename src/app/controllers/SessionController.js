7c76a31c881bd03a6645da5fa64bd74777c95680
Kelvin Montini <kelvin.montini@outlook.com>
2020-01-03 23:31:28 -0300

    Update de usuário + validação com Yup

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -1,10 +1,25 @@
 import jwt from 'jsonwebtoken';
+import * as Yup from 'yup';
 import User from '../models/User';
 
 import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'empty.required.fields',
+        message: 'Verifique se todos os campos foram preenchidos',
+      });
+    }
+
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,26 @@
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      phone: Yup.string().required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'empty.required.fields',
+        message: 'Verifique se todos os campos foram preenchidos',
+      });
+    }
+
     const emailExists = await User.findOne({
       where: { email: req.body.email },
     });
@@ -21,6 +40,73 @@ class UserController {
     const { id, name, email, phone } = await User.create(req.body);
 
     return res.json({ id, name, email, phone });
+  }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      phone: Yup.string(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({
+        error: 'empty.required.fields',
+        message: 'Verifique se todos os campos foram preenchidos',
+      });
+    }
+
+    const { email, phone, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return res.status(400).json({
+          error: 'user.duplicated',
+          message: 'E-mail informado já possuí cadastro, verifique seus dados',
+        });
+      }
+    }
+
+    if (phone !== user.phone) {
+      const userExists = await User.findOne({ where: { phone } });
+
+      if (userExists) {
+        return res.status(400).json({
+          error: 'user.duplicated',
+          message:
+            'Telefone informado já possuí cadastro, verifique seus dados',
+        });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({
+        error: 'invalid.credentials',
+        message: 'E-mail e/ou senha inválidos',
+      });
+    }
+
+    const { id, name } = await user.update(req.body);
+
+    return res.json({
+      id,
+      name,
+      email,
+      phone,
+    });
   }
 }
 
