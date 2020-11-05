c3ac17af9bc8033a2f026907679e50b950e00c2f
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2020-11-04 22:28:58 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: 'postgres',
   host: 'localhost',
   username: 'postgres',
-  password: '99845993',
+  password: 'aquicolocarasenhaza',
   database: 'unebd',
   define: {
     timestamps: true,
