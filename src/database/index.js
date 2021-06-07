17cb71dd392834a1d038551334ff043eb6439647
Lucas Pereira Cabral <lucaspereira142010@gmail.com>
2021-06-07 19:25:22 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,21 +1,5 @@
 import Sequelize from 'sequelize';
 
-import User from '../app/models/User';
-import Ativador from '../app/models/Ativador';
-import Pedidos from '../app/models/Pedidos';
-import CaixaUnes from '../app/models/CaixaUnes';
-import Entradas from '../app/models/Entradas';
-import EntradasAvulsas from '../app/models/EntradasAvulsas';
-import Saidas from '../app/models/Saidas';
-
-import databaseConfig from '../config/database';
-
-const models = [User, Ativador, Pedidos, CaixaUnes, Entradas,EntradasAvulsas, Saidas];
-
-class Database {
-  constructor() {
-    this.init();
-  }
 
   init() {
     this.connection = new Sequelize(databaseConfig);
